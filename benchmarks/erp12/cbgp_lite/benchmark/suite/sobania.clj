cfc0c58e4093b5a70b12799256f2964d3586a046
Tom Helmuth <thelmuth@hamilton.edu>
2026-01-20 16:12:54 -0500

    Maybe working, but probably not

--- benchmarks/erp12/cbgp_lite/benchmark/suite/sobania.clj
+++ benchmarks/erp12/cbgp_lite/benchmark/suite/sobania.clj
@@ -4,8 +4,7 @@
             [clojure.java.io :as io]
             [clojure.string :as str]
             [erp12.cbgp-lite.benchmark.utils :as bu]
-            [erp12.cbgp-lite.lang.lib :as lib]
-            [psb2.core :as psb2]))
+            [erp12.cbgp-lite.lang.lib :as lib]))
 
 (defn read-csv-from-filename
   [filename]
@@ -39,17 +38,20 @@
     {:input->type input->type
      :output-type output-type}))
 
+(defn get-parse-fn
+  [type]
+  (case type
+    "int" parse-long
+    "float" parse-double
+    "boolean" parse-boolean
+    "string" identity))
+
 (defn process-literal-pair
   "Given pair such as [\"9\" \"int\"], return literal as required by CBGP"
   [[value-str type]]
-  (let [parse-fn (case type
-                   "int" parse-long
-                   "float" parse-double
-                   "boolean" parse-boolean
-                   "string" identity)
+  (let [parse-fn (get-parse-fn type)
         value (parse-fn value-str)]
     {:gene :lit, :val value, :type (string-to-cbgp-type type)}))
-
 
 (defn get-vector-of-literals
   "Given problem file, return vector of literals as required by problem map"
@@ -68,7 +70,7 @@
   (let [{:keys [input->type output-type]} (get-inputs-and-outputs problem)]
     {problem
      {:description    "Generated problem"
-      :input->type    input->type
+      :input->type    input->type ;; TMH PROBLEM: inputs start with 0, where CBGP expects them to start with 1
       :ret-type       output-type
       :other-type-ctors    #{'double? 'int?}
       :extra-genes    (get-vector-of-literals problem)
@@ -78,16 +80,43 @@
                              {:type double?} [#(bu/round 4 (bu/absolute-distance %1 %2))]
                              {:type string?} [lev/distance]))}}))
 
+(defn read-cases
+  "Needs to take config map and return map of train and test cases."
+  [{:keys [problem]}]
+  (let [[train-header & train-data] (read-csv-from-filename
+                                     (str (io/file "sobania_data" problem "train.csv")))
+        [_ & test-data]             (read-csv-from-filename
+                                     (str (io/file "sobania_data" problem "test.csv")))
+        parse-fns (map #(get-parse-fn
+                         (last (str/split % #":")))
+                       train-header)
+        parse-set (fn [data] ;; function to parse and format train/test set using parse-fns
+                    (map (fn [row]
+                           (let [parsed-row (map #(%2 %1)
+                                                 row
+                                                 parse-fns)]
+                             {:inputs (vec (butlast parsed-row))
+                              :output (last parsed-row)}))
+                         data))
+        train (parse-set train-data)
+        test  (parse-set test-data)]
+    {:train train
+     :test  test}))
+
+
 (comment
+  
 
   (get-inputs-and-outputs "instance2_loc7_cc1_any")
-
+  
   (get-vector-of-literals "instance2_loc7_cc1_any")
-
+  
   lib/STRING
   
   (problems {:problem "instance2_loc7_cc1_any"
              :penalty 500})
+  
+  (read-cases {:problem "instance2_loc7_cc1_any"})
   
   
   )
@@ -95,25 +124,6 @@
 
 
 
-(defn reshape-case
-  [case {:keys [out-key stdout-key]
-         :or   {out-key :output1}}]
-  (merge
-   {:inputs (->> case
-                 (filter (fn [[k _]] (str/starts-with? (name k) "input")))
-                 (sort-by first)
-                 (mapv second))
-    :output (if (sequential? out-key)
-              (vec (map #(get case %) out-key))
-              (out-key case))}
-   (when stdout-key
-     {:std-out (stdout-key case)})))
-
-(defn read-cases
-  "Needs to take config map and return map of train and test cases."
-  [{:keys [data-dir problem n-train n-test]}]
-  (let [problem-info         (get (problems {}) (name problem))
-        reshape              #(reshape-case % problem-info)
-        {:keys [train test]} (psb2/fetch-examples (str data-dir) (str problem) n-train n-test)]
-    {:train (map reshape train)
-     :test  (map reshape test)}))
+;; TODO:
+;; x - get read-cases working
+;; - see TMH PROBLEM
